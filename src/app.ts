124e99c6043c6fa40f8ef56dce48908226453ab2
brkygngr <berkaygungor.1996@gmail.com>
2024-07-29 22:49:59 +0300

    chore(config): configure postgre db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,13 @@
 import express from 'express';
+import { AppDataSource } from './config/database';
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
 
 const app = express();
 
