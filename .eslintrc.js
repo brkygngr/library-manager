24f2574f326fca4fc460cc6158d6c05c8489aa7a
brkygngr <berkaygungor.1996@gmail.com>
2024-07-30 23:41:58 +0300

    chore: add ignored eslintrc and jest configs to version control

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
+
+export default config;
